50417ad87b914837c3c108f1e0bb07496d476d90
DESKTOP-P1JDINO\POOH <gzhenjin419@gmail.com>
2018-03-10 10:48:28 +0800

    update

---
+++ Source/Assets/Scripts/CoinRotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinRotate : MonoBehaviour {
+
+    [SerializeField]
+    private float RotatoionSpeed = 100;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0, Time.deltaTime * RotatoionSpeed, 0));
+    }
+}
